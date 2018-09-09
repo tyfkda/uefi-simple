bcb03e6681274742189f3be62365f9240e475d72
tyfkda <tyfkda@gmail.com>
2018-09-09 17:22:09 +0900

    Change code style

--- main.c
+++ main.c
@@ -2,37 +2,32 @@
 #define EFIAPI
 #define EFI_SUCCESS 0
 
-typedef unsigned short CHAR16;
+#ifndef wchar_t
+#define wchar_t wchar_t
+typedef __WCHAR_TYPE__ wchar_t;
+#endif
+
+typedef wchar_t CHAR16;
 typedef unsigned long long EFI_STATUS;
 typedef void *EFI_HANDLE;
 
-struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
-typedef
-EFI_STATUS
-(EFIAPI *EFI_TEXT_STRING) (
-    IN struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL  *This,
-    IN CHAR16                                   *String
-    );
 
-typedef struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL {
-    void             *a;
-    EFI_TEXT_STRING  OutputString;
+typedef struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL {
+  void             *a;
+  EFI_STATUS (EFIAPI *OutputString)(
+      IN struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL  *This,
+      IN CHAR16                                  *String);
 } EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
 
 typedef struct {
-    char                             a[52];
-    EFI_HANDLE                       ConsoleOutHandle;
-    EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL  *ConOut;
+  char                             a[52];
+  EFI_HANDLE                       ConsoleOutHandle;
+  EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL  *ConOut;
 } EFI_SYSTEM_TABLE;
 
-EFI_STATUS
-EFIAPI
-EfiMain (
-    IN EFI_HANDLE        ImageHandle,
-    IN EFI_SYSTEM_TABLE  *SystemTable
-    )
-{
-    SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Hello World!\n");
-    while(1);
-    return EFI_SUCCESS;
+EFI_STATUS EFIAPI
+EfiMain(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE *SystemTable) {
+  SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Hello World!\r\n");
+  while(1);
+  return EFI_SUCCESS;
 }
